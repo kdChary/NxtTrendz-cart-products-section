5b5862dfbdc0bccdd3f52058f56c7a2d34f49ab6
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-27 15:46:58 +0000

    cart component coded

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route, Switch} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -10,6 +11,7 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
     <Route exact path="/" component={Home} />
+    <Route exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
